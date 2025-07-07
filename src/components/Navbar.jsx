14435a81238fad3206230e99fb50a5e0c7717bb2
antharya01 <159692927+anthu05@users.noreply.github.com>
2025-07-08 00:02:42 +0530

    commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="nav flex items-center justify-center h-[80px] bg-zinc-900 px-[100px]">
       <div className="logo flex items-center gap-[10px]">
         <Bot size={50} color="#a855f7" />
-        <span className="text-2xl font-bold text-white ml-2">coditor</span>
+        <span className="text-2xl font-bold text-white ml-2">Coditor</span>
       </div>
     </div>
   );
